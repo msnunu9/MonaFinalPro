472cf559d6f09739445f6aa2c353d41adeca089b
Owner <alsalamcse>
2022-12-01 11:36:08 +0200

    SIGN UP SCREEN finish splash screen,sign in,up,menu 2 background done

--- app/src/main/java/sn/mona/monafinalpro/AddMedecine.java
+++ app/src/main/java/sn/mona/monafinalpro/AddMedecine.java
@@ -1,8 +1,11 @@
 package sn.mona.monafinalpro;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -95,6 +98,41 @@ public class AddMedecine extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 imageChooser();
+            }
+
+            private void imageChooser()
+            {
+                // create an instance of the
+                // intent of the type image
+                Intent i=new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                // pass the constant to compare it
+                // with the returned requestCode
+               startActivityForResult(Intent.createChooser(i,"select picture"),200);
+
+            }
+            // this function is triggered when user
+            // selects the image from the imageChooser
+            public void onActivityResult(int requestCode,int resultCode,Intent data)
+            {
+                AddMedecine.super.onActivityResult(requestCode,resultCode,data);
+
+                if (resultCode==RESULT_OK)
+                {
+                    // compare the resultCode with the
+                    // SELECT_PICTURE constant
+                    if (requestCode==200)
+                    {
+                        // Get the url of the image from data
+                        Uri selectedImageUri=data.getData();
+                        if (null!=selectedImageUri)
+                        {
+                            // update the preview image in the layout
+                            imgbtncam.setImageURI(selectedImageUri);
+                        }
+                    }
+                }
             }
         });
     }
